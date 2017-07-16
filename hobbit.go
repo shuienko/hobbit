6f2b8f73c41d3e83121ee8b173e31eb7386dd74c
Oleksandr Shuienko <Oleksandr_Shuienko@epam.com>
2017-07-16 14:15:00 +0300

    Fixed issue with wrong path declaration on systems without HOME environment variable. Refactoring.

--- hobbit.go
+++ hobbit.go
@@ -1,182 +1,207 @@
-// Extremely simple console tool.
-// Takes Long URL as a first argument.
-// Returns Bitly short URL to console.
-// That's It.
+// Extremely simple console tool. Takes Long URL as a first argument.
+// Returns Bitly short URL to console. That's It.
 package main
 
 import (
-      "os"
-      "log"
-      "fmt"
-      "regexp"
-      "io/ioutil"
-      "net/http"
-      "net/url"
-      "encoding/base64"
+    "os"
+    "log"
+    "fmt"
+    "regexp"
+    "io/ioutil"
+    "net/http"
+    "net/url"
+    "encoding/base64"
 )
 
 const(
-      API_BASEURL = "https://api-ssl.bitly.com"
-      TOKEN_DIR   = ".hobbit"
-      TOKEN_FILE  = "token"
+    API_BASEURL     = "https://api-ssl.bitly.com"
+    API_AUTH        = "/oauth/access_token"
+    API_SHORT       = "/v3/shorten"
+    TOKEN_FILE      = ".hobbit_token"
 
-      API_AUTH    = "/oauth/access_token"
-      API_SHORT   = "/v3/shorten"
-)
-
-var(
-    HOME = os.Getenv("HOME")
+    API_TOKEN_ERROR_INVALID = "INVALID_ARG_ACCESS_TOKEN"
+    API_TOKEN_ERROR_MISSING = "MISSING_ARG_ACCESS_TOKEN"
 )
 
 
-// Config file will be created under given path with given filename
-// Returns *os.File object and error
-func InitConfig(path string, filename string) (*os.File, error) {
-  if len(HOME) == 0 {
-    log.Println("HOME environment variable is empty. I'll use current folder")
-    config_file, err := os.Create(TOKEN_FILE)
+// Create config file and write token to It
+func SaveConfig(filename string, tk string) error {
+    HOME := os.Getenv("HOME")
+    var path string = ""
+
+    // If HOME is empty
+    if len(HOME) == 0 {
+      log.Println("HOME environment variable is empty. I'll use current folder")
+      path = filename
+      config_file, err := os.Create(path)
+      defer config_file.Close()
+      if err != nil {
+        log.Println(err)
+        return err
+      }
+
+      _, err = config_file.WriteString(tk)
+      if err != nil {
+        log.Println(err)
+        return err
+      }
+
+      return nil
+  }
+
+    // Set Path if HOME is not empty
+    path = HOME + "/" + filename
+
+    // Create config file and write token to It
+    config_file, err := os.Create(path)
+    defer config_file.Close()
+
     if err != nil {
-      return nil, err
+      log.Println(err)
+      log.Fatal("Can't create config file: ", path, " Check free space and permissions")
+    }
+
+    _, err = config_file.WriteString(tk)
+    if err != nil {
+      log.Println(err)
+      return err
+    }
+
+    return nil
+}
+
+
+// Get Token from file
+func ReadToken(filename string) string {
+    HOME := os.Getenv("HOME")
+    var path string = ""
+
+    if len(HOME) == 0 {
+      path = filename
     } else {
-      return config_file, nil
+      path = HOME + "/" + filename
     }
-  }
 
-  // Define absolute path to config
-  config_dir       := HOME + "/" + TOKEN_DIR
-  config_file_path := config_dir + "/" + TOKEN_FILE
+    f, err := os.Open(path)
+    defer f.Close()
 
-  // Create config directory if not exists
-  if _, err := os.Stat(config_dir); os.IsNotExist(err) {
-    err := os.Mkdir(config_dir, 0755)
     if err != nil {
       log.Println(err)
-      log.Fatal("can't create directory: ", config_dir ," Check free space and permissions")
+      return ""
     }
-  }
 
-  // Create config file if not exists
-  if _, err := os.Stat(config_file_path); os.IsNotExist(err) {
-    config_file, err := os.Create(config_file_path)
-    if err != nil {
-      log.Println(err)
-      log.Fatal("can't create config file: ", config_file_path, " Check free space and permissions")
-    }
-    return config_file, err
-  }
+    stat, _ := f.Stat()
+    bs := make([]byte, stat.Size())
+    f.Read(bs)
 
-  return os.Open(config_file_path)
-}
-
-// Save Token on a filesystem
-func SaveToken(tk string, f *os.File) (int, error) {
-  return f.WriteString(tk)
+    return string(bs)
 }
 
 
-// Get access_token
+// Authenticate. Returns access_token
 func Auth() string {
-  username := ""
-  password := ""
+    username := ""
+    password := ""
 
-  // Get username and password from user
-  fmt.Println("Looks like access_token is not set. Let's do It!")
-  fmt.Printf("%s: ","username")
-  fmt.Scanln(&username)
-  fmt.Printf("%s: ", "password")
-  fmt.Scanln(&password)
+    // Get username and password from user
+    fmt.Println("Bitly access_token is not set. Let's do It!")
+    fmt.Printf("%s: ","username")
+    fmt.Scanln(&username)
+    fmt.Printf("%s: ", "password")
+    fmt.Scanln(&password)
 
-  // Create request
-  client := &http.Client{}
-  urlStr := API_BASEURL + API_AUTH
-  r, err := http.NewRequest("POST", urlStr, nil)
-  if err != nil {
-    log.Fatal(err)
-  }
-  // Set auth header
-  msg := username + ":" + password
-  auth_header := "Basic " + base64.StdEncoding.EncodeToString([]byte(msg))
-  r.Header.Add("Authorization", auth_header)
+    // Create request
+    client := &http.Client{}
+    urlStr := API_BASEURL + API_AUTH
+    r, err := http.NewRequest("POST", urlStr, nil)
+    if err != nil {
+      log.Fatal(err)
+    }
 
-  // Get response
-  resp, err := client.Do(r)
-  if err != nil {
-    log.Fatal(err)
-  }
-  defer resp.Body.Close()
+    // Set auth header
+    msg := username + ":" + password
+    auth_header := "Basic " + base64.StdEncoding.EncodeToString([]byte(msg))
+    r.Header.Add("Authorization", auth_header)
 
-  // Get token
-  body, err := ioutil.ReadAll(resp.Body)
-  if err != nil {
-    log.Fatal(err)
-  }
+    // Get response
+    resp, err := client.Do(r)
+    if err != nil {
+      log.Fatal(err)
+    }
+    defer resp.Body.Close()
 
-  // Check errors and return access_token
-  response_string := string(body)
-  _, err = regexp.MatchString("[a-zA-Z0-9]+", response_string)
-  if err != nil {
-    log.Fatal(response_string)
-  }
-  return response_string
+    // Get token
+    body, err := ioutil.ReadAll(resp.Body)
+    if err != nil {
+      log.Fatal(err)
+    }
+
+    // Check errors and return access_token
+    response_string := string(body)
+    _, err = regexp.MatchString("[a-zA-Z0-9]+", response_string)
+    if err != nil {
+      log.Fatal(response_string)
+    }
+    return response_string
 }
 
 
 // Shorten long URL
 func Shorten(tk string ,longurl string) string {
-  urlStr := API_BASEURL + API_SHORT
+    urlStr := API_BASEURL + API_SHORT
 
-  Url, err := url.Parse(urlStr)
-  if err != nil {
-    log.Fatal(err)
-  }
+    Url, err := url.Parse(urlStr)
+    if err != nil {
+      log.Fatal(err)
+    }
 
-  // Set parameters
-  parameters := url.Values{}
-  parameters.Add("access_token", tk)
-  parameters.Add("longUrl", longurl)
-  parameters.Add("format", "txt")
-  Url.RawQuery = parameters.Encode()
+    // Set parameters
+    parameters := url.Values{}
+    parameters.Add("access_token", tk)
+    parameters.Add("longUrl", longurl)
+    parameters.Add("format", "txt")
+    Url.RawQuery = parameters.Encode()
 
-  // Call API endpoint
-  resp, err := http.Get(Url.String())
-  if err != nil {
-    log.Fatal(err)
-  }
-  defer resp.Body.Close()
+    // Call API endpoint
+    resp, err := http.Get(Url.String())
+    if err != nil {
+      log.Fatal(err)
+    }
+    defer resp.Body.Close()
 
-  body, err := ioutil.ReadAll(resp.Body)
-  if err != nil {
-    log.Fatal(err)
-  }
+    body, err := ioutil.ReadAll(resp.Body)
+    if err != nil {
+      log.Fatal(err)
+    }
 
-  return string(body)
+    return string(body)
 }
 
 
 func main() {
-  var token, longUrl string
+    var shortUrl, longUrl string
+    token := ReadToken(TOKEN_FILE)
 
-  f, _ := InitConfig(TOKEN_DIR, TOKEN_FILE)
-  defer f.Close()
+    // Check argumets
+    if len(os.Args) < 2 {
+      fmt.Println("USAGE:", os.Args[0], "http://example.com")
+      fmt.Println("NOTE:  'http/https' part is obligatory")
+      os.Exit(1)
+    }
 
-  // Read existing token from file or get a new one
-  stat, _ := f.Stat()
-  if stat.Size() > 0 {
-    bs := make([]byte, stat.Size())
-    f.Read(bs)
-    token = string(bs)
-  } else {
-    token = Auth()
-    SaveToken(token, f)
-  }
+    longUrl = os.Args[1]
 
-  // Check if argument has been passed to the script
-  if len(os.Args) < 2 {
-    fmt.Println("USAGE:", os.Args[0], "http://example.com")
-    fmt.Println("NOTE:  'http/https' part is obligatory")
-    os.Exit(1)
-  }
+    // Authenticate and save token. 3 attempts
+    for i := 0; i < 3; i++ {
+      shortUrl = Shorten(token, longUrl)
+      if shortUrl == API_TOKEN_ERROR_INVALID || shortUrl == API_TOKEN_ERROR_MISSING {
+        log.Println("Bitly access_token is not set/valid")
+        token = Auth()
+      } else {
+        fmt.Printf(shortUrl)
+        break
+      }
+    }
 
-  longUrl = os.Args[1]
-  fmt.Printf(Shorten(token, longUrl))
+    SaveConfig(TOKEN_FILE, token)
 }
